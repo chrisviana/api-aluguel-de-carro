12cd6c52631b538465408ee99e6bce4163f0bb9d
Christian Viana <viana2015christian@gmail.com>
2022-12-07 16:05:48 -0300

    Projeto API alguel de carro

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
+
+const app = express();
+app.use(express.json())
+
+app.use("/categories", categoriesRoutes)
+
+app.get("/", (req, res) => {
+    return res.json({ message: "Hello World!" })
+})
+
+app.listen(3333)
